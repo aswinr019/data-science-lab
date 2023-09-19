1e6d1fe31d73bb44fd770a7e7aad2dcbcc78fdb3
Aswin R <2227@tkmce.ac.in>
2023-09-19 11:45:12 +0530

    Added CO1 program2

---
+++ CO1/program2.py
@@ -0,0 +1,15 @@
+# Write a python program to find the number off uppercase and lowercase letters in a given text.
+
+text = input("Enter a text: ")
+
+upper_case_count = lower_case_count = 0
+
+for char in text: 
+    if(char.isupper()):
+        upper_case_count+= 1
+    else:
+        lower_case_count+=1
+        
+print("Given text: ",text)
+print("Number of uppercase characters in given text: ",upper_case_count)
+print("Number of lowercase characters in given text: ",lower_case_count)
