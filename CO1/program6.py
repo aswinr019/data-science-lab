c39cc74e911bd465eeb1d655593328b6863a0b80
aswin r <119281218+aswinr019@users.noreply.github.com>
2023-09-26 06:56:01 +0530

    Add files via upload

--- CO1/program6.py
+++ CO1/program6.py
@@ -4,3 +4,26 @@
 # wordlist which have name as a substring in it. The second element of
 # the tuple is a list showing the index at which the name occurs in each of
 # the words of the wordlist and a 0 if it doesn’t occur.
+
+def find_words_with_substring(wordlist, name):
+    
+    count = 0
+    indexes = []
+    
+    for word in wordlist:
+        if name in word:
+            count += 1
+            indexes.append(word.index(name)+1)
+        else:
+            indexes.append(0) 
+    output_tuple = (count, indexes)
+    return output_tuple
+
+wordlist=[]
+num = int(input("Enter the number of words"))
+for i in range(0,num):
+    print("Enter the word ",i+1)
+    wordlist.append(input())
+name = input("Enter the string")
+result = find_words_with_substring(wordlist, name)
+print(result)
