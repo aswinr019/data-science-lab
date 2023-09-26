a9a958a7497980ae8ae613c0b5688da7ad98468c
aswin r <119281218+aswinr019@users.noreply.github.com>
2023-09-26 06:55:13 +0530

    Add files via upload

---
+++ CO1/program9.py
@@ -0,0 +1,7 @@
+# Given a file “mark.csv” of student data with the fields rollno, name,
+# branch, m1, m2, m3, write python code to
+# •Print total marks of all students
+# •Find the average mark of each subject
+# • Find the student with highest and second highest mark.
+
+
